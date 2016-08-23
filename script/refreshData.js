23c54dbd10e512b1401736ac44d4e24e6ff52161
trekerz <zjf007@qq.com>
2016-08-24 00:12:52 +0800

    修改了土壤项布局和后四项

--- script/refreshData.js
+++ script/refreshData.js
@@ -10,20 +10,26 @@ document.getElementById("navIcon").onclick = function(){
 				var data = JSON.parse(request.responseText);
 				if (data.success) {
 					var msgGroup  = data.msg.split(",");
-					document.getElementById("valueSun").innerHTML = msgGroup[0];
-					document.getElementById("valueCarbonD").innerHTML = msgGroup[1];
-					document.getElementById("valueAir1Degree").innerHTML = msgGroup[2];
-					document.getElementById("valueAir1Percent").innerHTML = msgGroup[3];
-					document.getElementById("valueAir2Degree").innerHTML = msgGroup[4];
-					document.getElementById("valueAir2Percent").innerHTML = msgGroup[5];
-					document.getElementById("valueAir3Degree").innerHTML = msgGroup[6];
-					document.getElementById("valueAir3Percent").innerHTML = msgGroup[7];
-					document.getElementById("valueAir4Degree").innerHTML = msgGroup[8];
-					document.getElementById("valueAir4Percent").innerHTML = msgGroup[9];
-					document.getElementById("valueSoil1Degree").innerHTML = msgGroup[10];
-					document.getElementById("valueSoil2Percent").innerHTML = msgGroup[11];
-					document.getElementById("valueSoil1Degree").innerHTML = msgGroup[12];
-					document.getElementById("valueSoil2Percent").innerHTML = msgGroup[13];
+					var id = null;
+					for(var i = 0;i <= 13;i++){
+						id = getId(i);
+						document.getElementById(id).innerHTML = msgGroup[i];
+					}
+					for(var i = 14;i <= 22;i++){ //此处可按照情况修改为msgGroup.length+1，下面定义的id也可根据每栏列表的项目数而作优化
+						if(i>=14 && i<=17){
+							id = "valueOther1" + (i-13);
+							showStatus(i,id,msgGroup);
+						}else if(i>=18 && i<=19){
+							id = "valueOther2" + (i-17);
+							showStatus(i,id,msgGroup);
+						}else if(i>=20 && i<=21){
+							id = "valueOther3" + (i-19);
+							showStatus(i,id,msgGroup);
+						}else{
+							id = "valueOther41";
+							showStatus(i,id,msgGroup);
+						}
+					}
 				}else{
 					alert("刷新出错1：");
 				}
@@ -31,5 +37,41 @@ document.getElementById("navIcon").onclick = function(){
 				alert("刷新出错2：" + request.status);
 			}
 		}
+	}
+}
+
+function getId(index){
+	switch(index){
+		case 0:return "valueSun";break;
+		case 1:return "valueCarbonD";break;
+		case 2:return "valueAir1Degree";break;
+		case 3:return "valueAir1Percent";break;
+		case 4:return "valueAir2Degree";break;
+		case 5:return "valueAir2Percent";break;
+		case 6:return "valueAir3Degree";break;
+		case 7:return "valueAir3Percent";break;
+		case 8:return "valueAir4Degree";break;
+		case 9:return "valueAir4Percent";break;
+		case 10:return "valueSoil1Degree";break;
+		case 11:return "valueSoil1Percent";break;
+		case 12:return "valueSoil2Degree";break;
+		case 13:return "valueSoil2Percent";break;
+		default:break;
+	}
+}
+
+function showStatus(index,id,msgGroup){
+	if(msgGroup[index] == "w"){
+		var thisId = document.getElementById(id);
+		thisId.innerHTML = "未知";
+		thisId.parentNode.className += " unknown";
+	}else if(msgGroup[index] == "g"){
+		var thisId = document.getElementById(id);
+		thisId.innerHTML = "关闭";
+		thisId.parentNode.className += " closed";
+	}else{
+		var thisId = document.getElementById(id);
+		thisId.innerHTML = "全开";
+		thisId.parentNode.className += " opened";
 	}
 }
--- script/refreshJSON.php
+++ script/refreshJSON.php
@@ -5,9 +5,9 @@ header('Access-Control-Allow-Credentials:true');
 header("Content-Type: application/json;charset=utf-8"); 
 
 
-$staff = array
+$item = array
 	(
-		array("sun" => "29.8","carbonD" => "10.5","air1Degree" => "30.5","air1Percent" => "76","air2Degree" => "29.8","air2Percent" => "70","air3Degree" => "30.9","air3Percent" => "78","air4Degree" => "28.4","air4Percent" => "67","soil1Degree" => "28.5","soil1Percent" => "77","soil2Degree" => "28.8","soil2Percent" => "79")
+		array("sun" => "29.8","carbonD" => "10.5","air1Degree" => "30.5","air1Percent" => "76","air2Degree" => "29.8","air2Percent" => "70","air3Degree" => "30.9","air3Percent" => "78","air4Degree" => "28.4","air4Percent" => "67","soil1Degree" => "29.5","soil1Percent" => "77","soil2Degree" => "30.4","soil2Percent" => "79","other11" => "w","other12" => "g","other13" => "g","other14" => "q","other21" => "q","other22" => "w","other31" => "w","other32" => "w","other41" => "q")
 	);
 
 if($_SERVER["REQUEST_METHOD"] == "GET"){
@@ -15,11 +15,11 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 }
 
 function refresh(){
-	global $staff;
+	global $item;
 
-	foreach ($staff as $value){
+	foreach ($item as $value){
 		$result = '{"success":true,
-					"msg":"' . $value["sun"] . ',' . $value["carbonD"] . ',' . $value["air1Degree"] . ',' . $value["air1Percent"] . ',' . $value["air2Degree"] . ',' . $value["air2Percent"] . ',' . $value["air3Degree"] . ',' . $value["air3Percent"] . ',' . $value["air4Degree"] . ',' . $value["air4Percent"] . ',' . $value["soil1Degree"] . ',' . $value["soil1Percent"] . ',' . $value["soil2Degree"] . ',' . $value["soil2Percent"] . '"}';
+					"msg":"' . $value["sun"] . ',' . $value["carbonD"] . ',' . $value["air1Degree"] . ',' . $value["air1Percent"] . ',' . $value["air2Degree"] . ',' . $value["air2Percent"] . ',' . $value["air3Degree"] . ',' . $value["air3Percent"] . ',' . $value["air4Degree"] . ',' . $value["air4Percent"] . ',' . $value["soil1Degree"] . ',' . $value["soil1Percent"] . ',' . $value["soil2Degree"] . ',' . $value["soil2Percent"] . ',' . $value["other11"] . ',' . $value["other12"] . ',' . $value["other13"] . ',' . $value["other14"] . ',' . $value["other21"] . ',' . $value["other22"] . ',' . $value["other31"] . ',' . $value["other32"] . ',' . $value["other41"] . '"}';
 	}
 	echo $result;
 }
